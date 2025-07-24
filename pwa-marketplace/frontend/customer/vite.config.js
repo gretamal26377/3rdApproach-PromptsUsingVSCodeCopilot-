6fce61fd88b194c88724b813908e8d9e4a21b626
Gonzalo Retamales Lermanda <gretamal263@hotmail.com>
2025-07-24 00:33:56 -0400

    129th Commit: Moving from CRA to Vite Apps (customer: Update README, package.json, and vite.config.js)

--- pwa-marketplace/frontend/customer/vite.config.js
+++ pwa-marketplace/frontend/customer/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
-export default defineConfig({
+// ESM-compatible __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Always import from 'shared-lib'
+      "shared-lib":
+        mode === "development"
+          ? path.resolve(__dirname, "../shared-lib/src")
+          : path.resolve(__dirname, "../shared-lib/dist"),
+    },
+  },
+}));

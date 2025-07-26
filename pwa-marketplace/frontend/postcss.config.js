2298e373666ba55785aa4b60593670fa718e844f
Gonzalo Retamales Lermanda <gretamal263@hotmail.com>
2025-07-25 21:11:17 -0400

    132th Commit: Moving to use Workspace Centralised Setup between shared-lib local package and customer & admin's frontend

---
+++ pwa-marketplace/frontend/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    // Changed after persistent error, reading myself error description.
+    // GitHub Copilot couldn't help me after several attempts
+    // "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
